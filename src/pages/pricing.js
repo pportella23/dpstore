3510d3566d2e841eedbabab53bea706ad8d40bb2
Pedro Portella <pportella23@gmail.com>
2023-08-24 11:13:31 -0300

    feat: fetch plan prices from Stripe

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,1 +1,28 @@
-export default function PricingPage() {}
+import { stripe } from "src/pricing/utils/stripe";
+
+export default function PricingPage({ plans }) {
+  console.log(plans);
+
+  return <div />;
+}
+
+export async function getStaticProps() {
+  const { data: prices } = await stripe.prices.list();
+  const plans = [];
+
+  for (const price of prices) {
+    const product = await stripe.products.retrieve(price.product);
+    plans.push({
+      name: product.name,
+      id: price.id,
+      price: price.unit_amount / 100,
+      interval: price.recurring.interval,
+    });
+  }
+
+  return {
+    props: {
+      plans,
+    },
+  };
+}
