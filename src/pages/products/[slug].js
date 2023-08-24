a42172070956aba37bddd65c5ebc4575ac7dbebd
Pedro Portella <pportella23@gmail.com>
2023-08-24 09:49:32 -0300

    feat: Create product page

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,9 +1,35 @@
+import Image from "next/image";
+import PromoCard from "src/products/components/PromoCard";
 import { supabase } from "supabase";
 
 export default function ProductPage({ product }) {
-  console.log(product);
-
-  return <div>product page</div>;
+  return (
+    <section className="product-section">
+      <article className="product">
+        <div className="product-wrap">
+          <Image
+            width={1000}
+            height={300}
+            src={`/assets/${product.slug}.png`}
+            alt={product.name}
+          />
+        </div>
+        <section>
+          <header>
+            <h3>{product.name}</h3>
+          </header>
+          <section>
+            <div>
+              <p>{product.description}</p>
+            </div>
+          </section>
+        </section>
+        <section>
+          <PromoCard />
+        </section>
+      </article>
+    </section>
+  );
 }
 
 export async function getStaticPaths() {
