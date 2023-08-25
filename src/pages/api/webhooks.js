ae6df80c8a989a37fe64154e902240789841c257
Pedro Portella <pportella23@gmail.com>
2023-08-24 22:03:37 -0300

    feat: Add subscription update event hook & small fixes

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -1,5 +1,6 @@
 import getRawBody from "raw-body";
 import { stripe } from "src/pricing/utils/stripe";
+import { supabase } from "supabase";
 
 export const config = {
   api: {
@@ -21,7 +22,60 @@ export default async function handler(req, res) {
     return res.status(400).end();
   }
 
-  console.log(event);
+  try {
+    switch (event.type) {
+      case "customer.subscription.updated":
+        await updateSubscription(event);
+        break;
+      case "customer.subscription.deleted":
+        await deleteSubscription(event);
+        break;
+    }
 
-  res.send({ success: true });
+    res.send({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.send({ success: false });
+  }
 }
+
+async function updateSubscription(event) {
+  const subscription = event.data.object;
+  const stripe_customer_id = subscription.customer;
+  const subscription_status = subscription.status;
+  const price = subscription.items.data[0].price.id;
+  const { data: profile } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("stripe_customer_id", stripe_customer_id)
+    .single();
+
+  if (profile) {
+    const updatedSubscription = {
+      subscription_status,
+      price,
+    };
+    await supabase
+      .from("profile")
+      .update(updatedSubscription)
+      .eq("stripe_customer_id", stripe_customer_id);
+  } else {
+    const customer = await stripe.customers.retrieve(stripe_customer_id);
+    const name = customer.name;
+    const email = customer.email;
+    const newProfile = {
+      name,
+      email,
+      stripe_customer_id,
+      subscription_status,
+      price,
+    };
+    await supabase.auth.admin.createUser({
+      email,
+      email_confirm: true,
+      user_metadata: newProfile,
+    });
+  }
+}
+
+async function deleteSubscription(event) {}
