14796056ae7bf71cbcff10f0ec01952863e53823
Pedro Portella <pportella23@gmail.com>
2023-08-27 15:10:52 -0300

    feat: Handle product contents for subscribed users

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,18 +1,52 @@
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 import PromoCard from "src/products/components/PromoCard";
+import SubscriberCard from "src/products/components/SubscriberCard";
 import { supabase } from "supabase";
 
 export default function ProductPage({ product }) {
+  const supabaseClient = useSupabaseClient();
+  const session = useSession();
+  const [productContent, setProductContent] = useState(null);
+
+  useEffect(() => {
+    async function getProductContent() {
+      const { data: productContent } = await supabaseClient
+        .from("product_content")
+        .select("*")
+        .eq("id", product.product_content_id)
+        .single();
+      setProductContent(productContent);
+    }
+
+    getProductContent();
+  }, [supabaseClient]);
+
   return (
     <section className="product-section">
       <article className="product">
         <div className="product-wrap">
-          <Image
-            width={1000}
-            height={300}
-            src={`/assets/${product.slug}.png`}
-            alt={product.name}
-          />
+          {productContent?.download_url && (
+            <a
+              href={`/assets/${productContent.download_url}`}
+              download
+              className="download-link large-button"
+            >
+              <span className="large-button-text">Download</span>
+            </a>
+          )}
+          {productContent?.video_url ? (
+            <ReactPlayer controls url={productContent.video_url} />
+          ) : (
+            <Image
+              width={1000}
+              height={300}
+              src={`/assets/${product.slug}.png`}
+              alt={product.name}
+            />
+          )}
         </div>
         <section>
           <header>
@@ -24,9 +58,7 @@ export default function ProductPage({ product }) {
             </div>
           </section>
         </section>
-        <section>
-          <PromoCard />
-        </section>
+        <section>{session ? <SubscriberCard /> : <PromoCard />}</section>
       </article>
     </section>
   );
